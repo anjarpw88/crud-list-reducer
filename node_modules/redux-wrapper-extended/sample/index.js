db775631cd5b908df34ed4ad63b28bff8af45c16
Anjar <anjar.wicaksono@sellfazz.com>
2018-04-22 11:28:12 +0700

    remove node -modules

--- node_modules/redux-wrapper-extended/sample/index.js
+++
@@ -1,6 +0,0 @@
-require('babel-register');
-require('./sample-01-simple.js');
-require('./sample-02-changed-handler.js');
-require('./sample-03-cascading-reducers.js');
-require('./sample-04-mixin-reducers.js');
-require('./sample-05-action-collections.js');
